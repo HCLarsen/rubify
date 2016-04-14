295bda51fdaced49d1513dfd35b40a2326650d00
Chris Larsen <clarsenipod@gmail.com>
2016-04-14 01:35:02 -0400

    String Scanner

--- rubify.rb
+++ rubify.rb
@@ -2,4 +2,41 @@
 #
 # Runs through a ruby file, and changes all tabs to double spaces.
 
+require 'strscan'
+require 'byebug'
 
+filename = 'test.rb'
+count = 0
+
+if File.file? filename
+  if !File.readable?(filename)
+    puts "File #{filename} is not readable"
+    exit
+  end
+  if !File.writable?(filename)
+    puts "File #{filename} is not writable"
+    exit
+  end
+  @file = File.read(filename)
+
+  COMMENT = /#/
+  TAB = /\t/
+  SPACES = "  "
+
+  s = StringScanner.new(@file)
+
+  until s.eos?
+    if s.scan(TAB)
+      puts "tab found"
+      count += 1
+      # replace the tab with the SPACES string
+    elsif s.scan(COMMENT)
+      s.skip_until(/\n/)
+    else
+      s.getch
+    end
+  end
+  puts "#{count} tabs found"
+else
+  puts "File #{filename} doesn't exist"
+end
--- test.rb
+++ test.rb
@@ -2,8 +2,8 @@
 
 class TestClass
   def initialize
-    @string1 = "This is a string"
-    @string2 = "This is also a string"
+		@string1 = "This is a string"
+		@string2 = "This is also a string"
     @string3 = "This is a third string"
   end
 end
